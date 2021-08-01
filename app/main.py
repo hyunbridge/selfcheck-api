caa0f725ec919c4e6cc642c432952385a833be7a
Hyungyo Seo <51025374+hyunbridge@users.noreply.github.com>
2021-08-01 21:02:56 +0900

    Pass library response directly to http

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, status
 from pydantic import BaseModel
 
 import aiohttp
@@ -19,35 +19,19 @@ class SelfCheckUser(BaseModel):
 
 
 @app.post("/validate-user/")
-async def validate_user(user: SelfCheckUser, response: Response):
+async def validate_user(user: SelfCheckUser):
     async with aiohttp.ClientSession() as session:
         result = await hcskr.asyncUserLogin(user.name, user.birth, user.school_area, user.school_name,
                                             user.school_level, user.password, session)
-    if result["code"] == "SUCCESS":
-        return {
-            "success": True
-        }
-    else:
-        response.status_code = status.HTTP_400_BAD_REQUEST
-        return {
-            "success": False
-        }
+    return result
 
 
 @app.post("/self-check/")
-async def self_check(user: SelfCheckUser, response: Response):
+async def self_check(user: SelfCheckUser):
     result = await hcskr.asyncSelfCheck(user.name, user.birth, user.school_area, user.school_name, user.school_level,
                                         user.password)
-    if result["code"] == "SUCCESS":
-        return {
-            "success": True
-        }
-    else:
-        response.status_code = status.HTTP_400_BAD_REQUEST
-        return {
-            "success": False
-        }
+    return result
 
 
 if __name__ == "__main__":
-    uvicorn.run("server.api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
